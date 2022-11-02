6d12540d6c711a540532ca58562846659ad4520f
arogers <micheknows@gmail.com>
2022-11-02 14:16:38 -0600

    Testing

--- classworks/pull.php
+++ classworks/pull.php
@@ -2,6 +2,3 @@
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
 shell_exec('cd //classworks/.git && git pull');
-
-echo "payload = " . $_POST["payload"] . ".";
-echo "get payload = " . $_GET["payload"] . ".";
